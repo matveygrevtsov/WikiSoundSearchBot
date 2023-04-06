ed5a8bb56ef60636a62b8ee194c82bcdd5144e3b
matveygrevtsov <matveygrevcov@gmail.com>
2023-04-06 21:07:38 +0300

    Инициализация приложения

--- app-version-without-typescript/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const constants_1 = require("./constants");
-const telegraf_1 = require("telegraf");
-const bot = new telegraf_1.Telegraf("");
-console.log(constants_1.message);
