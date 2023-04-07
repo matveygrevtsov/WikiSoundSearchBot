645b77659fbf3ff361a81d1fca8b29dcf3c68db3
matveygrevtsov <matveygrevcov@gmail.com>
2023-04-07 11:47:39 +0300

    Добавил возможность использования env-переменных

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require("path");
+const Dotenv = require("dotenv-webpack"); // Плагин для использования .env-переменных.
 
 module.exports = {
   entry: "./src/index.ts",
-  target: "node",
+  target: "node", // Чтобы вебпак генерировал код для node.js-среды.
   mode: "production",
   module: {
     rules: [{ use: "ts-loader", exclude: /node_modules/ }],
@@ -14,4 +15,9 @@ module.exports = {
     filename: "index.js",
     path: path.resolve(__dirname, "build"),
   },
+  plugins: [
+    new Dotenv({
+      path: "./src/.env",
+    }),
+  ],
 };
