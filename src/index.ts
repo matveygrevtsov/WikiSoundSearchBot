50fd0bb37c06345abfb6b196881d9831af97b9b4
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2023-04-10 12:03:16 +0300

    Добавил web-hook (#5)
    Co-authored-by: matveygrevtsov <matveygrevcov@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
+import { launchConfig } from "./launchConfig";
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply("Welcome"));
 bot.help((ctx) => ctx.reply("Send me a sticker"));
 bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
 bot.hears("hi", (ctx) => ctx.reply("Hey there"));
-bot.launch();
+bot.launch(launchConfig);
 
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
