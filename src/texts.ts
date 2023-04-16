518e434ca5e97281976759741e4c873c6e82a62a
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2023-04-16 14:15:00 +0300

    Доработал функцию обрезания текста. (#13)
    * Доработал функцию обрезания текста
    
    * Доработал функцию обрезания текста
    
    ---------
    
    Co-authored-by: matveygrevtsov <matveygrevcov@gmail.com>

--- src/texts.ts
+++ src/texts.ts
@@ -1,10 +1,11 @@
 export const texts = {
-  greeting: "Привет!",
+  greeting:
+    "Здравствуйте! Напишите мне термин, определение которого я должен озвучить.",
   errors: {
     wikipediaError: "Произошла ошибка при запросе в Википедию.",
     wikipediaNotFound:
       "К сожалению, Википедия не смогла найти определение по запросу",
-    yandexSpeechkitError: "Произошла ошибка при запросе в yandex speechkit",
+    yandexSpeechkitError: "Произошла ошибка при запросе в yandex speechkit.",
     telegrafSendVoiceMessageError:
       "К сожалению, боту не удалось отправить вам голосовое сообщение.",
     notATextMessage: "Пожалуйста, отправьте текстовое сообщение.",
--- src/utils/cutText.ts
+++ src/utils/cutText.ts
@@ -1,12 +1,14 @@
+const TEXT_MAX_LENGTH = 300;
+
 /**
  * Обрезает текст, оставляя первые 300 символов до последней дочки.
  * @param text - текст, который будет обрезан.
  */
 export function cutText(text: string): string {
   let result = text.replaceAll("\n", " ");
-  if (result.length <= 300) return text;
-  result = result.slice(0, 300);
+  if (result.length <= TEXT_MAX_LENGTH) return text;
+  result = result.slice(0, TEXT_MAX_LENGTH);
   const indexOfLastDot = result.lastIndexOf(".");
-  if (indexOfLastDot === -1) return result;
+  if (indexOfLastDot < TEXT_MAX_LENGTH / 2) return result;
   return result.slice(0, indexOfLastDot);
 }
